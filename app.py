42e88f4e5cc982ba5c7f534aaa5a8f48f7e24840
Tiago Costa <tiago.nazareth.costa@tecnico.ulisboa.pt>
2025-11-01 13:12:18 +0000

    debug atividades completo outra vez

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ import uuid
 from icalendar import Calendar, Event
 from flask import make_response
 
-#MEGA ALTERAÇÂOOOOOOOOO
+#MEGA ALTERAÇÂOOOOOOOOOOO
 
 # --- IMPORTAÇÕES DE BASE DE DADOS ---
 from flask_sqlalchemy import SQLAlchemy 
